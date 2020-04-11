0a52063e3ba33287d6554635b4bbc929b94f0215
Shane de Silva (Imperial Laptop) <sdesilva26@gmail.com>
2020-04-11 20:31:54 +0100

    fix for scala installation

--- build_docker_images.cmd
+++ build_docker_images.cmd
@@ -1,10 +1,10 @@
-docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.1 .
-docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.1 .
-docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.1 .
+docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.2 .
+docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.2 .
+docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
 
-docker push sdesilva26/spark_master
-docker push  sdesilva26/spark_master:0.0.1
-docker push sdesilva26/spark_worker
-docker push sdesilva26/spark_worker:0.0.1
-docker push sdesilva26/spark_submit
-docker push sdesilva26/spark_submit:0.0.1
+docker push sdesilva26/spark_master:latest
+docker push  sdesilva26/spark_master:0.0.2
+docker push sdesilva26/spark_worker:latest
+docker push sdesilva26/spark_worker:0.0.2
+docker push sdesilva26/spark_submit:latest
+docker push sdesilva26/spark_submit:0.0.2
