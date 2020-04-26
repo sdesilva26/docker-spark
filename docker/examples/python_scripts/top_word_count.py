c487c8523107b5345acb0f76c948b407ee1fcab7
Shane de Silva (Imperial Laptop) <sdesilva26@gmail.com>
2020-04-26 17:45:31 +0100

    add Docker compose section to tutorial.md. Reformat top_word_count.py. Add docker-compose.yml for creating a docker stack

--- docker/examples/python_scripts/top_word_count.py
+++ docker/examples/python_scripts/top_word_count.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
 
 import sys
-from operator import add
 import time
+from operator import add
 
 from pyspark.sql import SparkSession
-
 
 if __name__ == "__main__":
 	if len(sys.argv) != 5:
@@ -13,30 +12,23 @@ if __name__ == "__main__":
 		      "AZURE_STORAGE_ACOCUNT_ACCESS_KEY CONTAINER_NAME FILE_NAME", file=sys.stderr)
 		sys.exit(-1)
 
-	spark = SparkSession \
-		.builder \
-		.appName("PythonWordCount") \
-		.getOrCreate()
+	spark = SparkSession.builder.appName("PythonWordCount").getOrCreate()
 
 	spark.conf.set("fs.azure.account.key.{}.blob.core.windows.net".format(sys.argv[1]),
-	               "{}".format(sys.argv[2]))
+		"{}".format(sys.argv[2]))
 	start_time = time.time()
-	lines = spark.read.text("wasbs://{}@{}.blob.core.windows.net/{}".format(sys.argv[3],
-	                                                                        sys.argv[1],sys.argv[
-		                                                                        4])).rdd.map(
-		lambda r: r[0])
-	counts = lines.flatMap(lambda x: x.split(' ')) \
-		.map(lambda x: (x, 1)) \
-		.reduceByKey(add) \
-		.map(lambda x: (x[1], x[0])) \
-		.sortByKey(False)
+	lines = spark.read.text(
+		"wasbs://{}@{}.blob.core.windows.net/{}".format(sys.argv[3], sys.argv[1],
+			sys.argv[4])).rdd.map(lambda r: r[0])
+	counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add).map(
+		lambda x: (x[1], x[0])).sortByKey(False)
 
 	output = counts.take(100)
 	print("The 100 most frequent words are :\n")
 	i = 1
 	for count, word in output:
 		print("{}. {} ---> {}".format(i, word, count))
-		i+=1
+		i += 1
 
 	end_time = time.time();
 
