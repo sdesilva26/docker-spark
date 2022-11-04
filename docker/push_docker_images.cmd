53453087bec2b5ec2a46217cbec708b9a7d5dd6b
Sergey <logistser@gmail.com>
2022-11-05 02:47:34 +0300

    versions up

--- docker/push_docker_images.cmd
+++ docker/push_docker_images.cmd
@@ -1,6 +1,4 @@
-docker push sdesilva26/spark_master:latest
-docker push  sdesilva26/spark_master:0.0.2
-docker push sdesilva26/spark_worker:latest
-docker push sdesilva26/spark_worker:0.0.2
-docker push sdesilva26/spark_submit:latest
-docker push sdesilva26/spark_submit:0.0.2
+docker push logistser/spark_base:latest
+docker push logistser/spark_master:latest
+docker push logistser/spark_worker:latest
+docker push logistser/spark_submit:latest
